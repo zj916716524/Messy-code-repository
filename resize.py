4f0bd865feed446d28adbb8224778689fa792826
方丈麻烦剃个头 <39594695+zj916716524@users.noreply.github.com>
2022-04-16 11:18:00 +0800

    Update resize.py

--- resize.py
+++ resize.py
@@ -2,20 +2,19 @@ import os
 import cv2
 
 
-# 修改图片大小
+# resize image
 def resize(img_path, save_path, size):
     img = cv2.imread(img_path)
     img = cv2.resize(img, (size, size))
     cv2.imwrite(save_path, img)
 
-
-img_path = '/home/zeng/AttentionGan_gujia/zhengkai_fangsong'
-save_path = '/home/zeng/AttentionGan_gujia/zhengkai_fangsong'
+img_path = ''
+save_path = ''
 for i in os.listdir(img_path):
-    if 'testA' in i or 'testB' in i:
-        img_box_path = os.path.join(img_path, i)
-        img_box_sava = os.path.join(save_path, i)
-        for j in os.listdir(img_box_path):
-            img_path_img = os.path.join(img_box_path, j)
-            img_save_sava = os.path.join(img_box_sava, j)
-            resize(img_path_img, img_save_sava, 256)
+    img_box_path = os.path.join(img_path, i)
+    img_box_sava = os.path.join(save_path, i)
+    for j in os.listdir(img_box_path):
+        img_path_img = os.path.join(img_box_path, j)
+        img_save_sava = os.path.join(img_box_sava, j)
+        # 256 is image size
+        resize(img_path_img, img_save_sava, 256)
